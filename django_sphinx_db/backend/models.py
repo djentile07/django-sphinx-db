5e75f3bc32f4c3d74f2509ef803e263cc0e55be0
Juan Carlos Moreno <jcrogel@gmail.com>
2017-07-12 15:30:45 -0700

    Despite the negative press covfefe 3

--- django_sphinx_db/backend/models.py
+++ django_sphinx_db/backend/models.py
@@ -53,7 +53,7 @@ class SphinxSearch(models.Lookup):
         params = lhs_params + rhs_params
         field = lhs.split(".")[-1] .replace("`", "")
         print "MATCH",rhs
-        return "MATCH ('@{} {}')".format(field, rhs), params
+        return "MATCH ('@{} {}')".format(field, params), []
 
 SphinxField.register_lookup(SphinxSearch)
 
