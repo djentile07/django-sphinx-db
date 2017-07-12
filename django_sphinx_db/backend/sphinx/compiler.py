4c6dcbe8b5eb4070dffcede93b2c0f49e7ed28ff
Juan Carlos Moreno <jcrogel@gmail.com>
2017-07-12 15:40:07 -0700

    Fucking bad idea by the maddog

--- django_sphinx_db/backend/sphinx/compiler.py
+++ django_sphinx_db/backend/sphinx/compiler.py
@@ -86,6 +86,103 @@ class SphinxQLCompiler(compiler.SQLCompiler):
         print "compiler - compile", retval
         return retval
 
+    def as_sql(self, with_limits=True, with_col_aliases=False, subquery=False):
+        """
+        Creates the SQL for this query. Returns the SQL string and list of
+        parameters.
+
+        If 'with_limits' is False, any limit/offset information is not included
+        in the query.
+        """
+        self.subquery = subquery
+        refcounts_before = self.query.alias_refcount.copy()
+        try:
+            extra_select, order_by, group_by = self.pre_sql_setup()
+            distinct_fields = self.get_distinct()
+
+            # This must come after 'select', 'ordering', and 'distinct' -- see
+            # docstring of get_from_clause() for details.
+            from_, f_params = self.get_from_clause()
+
+            where, w_params = self.compile(self.where) if self.where is not None else ("", [])
+            having, h_params = self.compile(self.having) if self.having is not None else ("", [])
+            params = []
+            result = ['SELECT']
+
+            if self.query.distinct:
+                result.append(self.connection.ops.distinct_sql(distinct_fields))
+
+            out_cols = []
+            col_idx = 1
+            for _, (s_sql, s_params), alias in self.select + extra_select:
+                if alias:
+                    s_sql = '%s AS %s' % (s_sql, self.connection.ops.quote_name(alias))
+                elif with_col_aliases:
+                    s_sql = '%s AS %s' % (s_sql, 'Col%d' % col_idx)
+                    col_idx += 1
+                params.extend(s_params)
+                out_cols.append(s_sql)
+
+            result.append(', '.join(out_cols))
+
+            result.append('FROM')
+            result.extend(from_)
+            params.extend(f_params)
+
+            if where:
+                result.append('WHERE %s' % where)
+                params.extend(w_params)
+
+            grouping = []
+            for g_sql, g_params in group_by:
+                grouping.append(g_sql)
+                params.extend(g_params)
+            if grouping:
+                if distinct_fields:
+                    raise NotImplementedError(
+                        "annotate() + distinct(fields) is not implemented.")
+                if not order_by:
+                    order_by = self.connection.ops.force_no_ordering()
+                result.append('GROUP BY %s' % ', '.join(grouping))
+
+            if having:
+                result.append('HAVING %s' % having)
+                params.extend(h_params)
+
+            if order_by:
+                ordering = []
+                for _, (o_sql, o_params, _) in order_by:
+                    ordering.append(o_sql)
+                    params.extend(o_params)
+                result.append('ORDER BY %s' % ', '.join(ordering))
+
+            if with_limits:
+                if self.query.high_mark is not None:
+                    result.append('LIMIT %d' % (self.query.high_mark - self.query.low_mark))
+                if self.query.low_mark:
+                    val = self.connection.ops.no_limit_value()
+                    if val:
+                        result.append('LIMIT %d, %d' % (self.query.low_mark, val))
+
+            if self.query.select_for_update and self.connection.features.has_select_for_update:
+                if self.connection.get_autocommit():
+                    raise TransactionManagementError(
+                        "select_for_update cannot be used outside of a transaction."
+                    )
+
+                # If we've been asked for a NOWAIT query but the backend does
+                # not support it, raise a DatabaseError otherwise we could get
+                # an unexpected deadlock.
+                nowait = self.query.select_for_update_nowait
+                if nowait and not self.connection.features.has_select_for_update_nowait:
+                    raise DatabaseError('NOWAIT is not supported on this database backend.')
+                result.append(self.connection.ops.for_update_sql(nowait=nowait))
+
+            return ' '.join(result), tuple(params)
+        finally:
+            # Finally do cleanup - get rid of the joins we created above.
+            self.query.reset_refcounts(refcounts_before)
+
 # Set SQLCompiler appropriately, so queries will use the correct compiler.
 SQLCompiler = SphinxQLCompiler
 
